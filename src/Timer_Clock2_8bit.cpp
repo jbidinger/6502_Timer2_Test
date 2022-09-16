6fba9a1b9d2b59ed7089cc6275b161810ef6499c
jon bidinger <jon@bidinger.org>
2022-09-15 20:45:54 -0500

    Added byte to padded binary function

--- src/Timer_Clock2_8bit.cpp
+++ src/Timer_Clock2_8bit.cpp
@@ -11,6 +11,8 @@
 #define MODERUNSTOP     2
 unsigned int mode=MODESINGLESTEP;
 
+//void binaryToPaddedString(char *, byte);
+
 // Note that the prescale value is just for printing; changing it here
 // does not change the clock division ratio for the timer!  To change
 // the timer prescale division, use different bits for CS22:0 below
@@ -24,6 +26,33 @@ const float freq      = 1.0e6 / period;
 
 #define LED_PIN LED_BUILTIN
 
+void binaryToPaddedString(char *output, byte inputbinary) {
+  int targetStrLen = 8; 
+  char unpadded[20];
+  itoa (inputbinary,unpadded,2); 
+
+    char buf[21] = { 0 };
+    char fill = '0';
+  
+
+    sprintf(output, "%s%s", (char*)memset(buf, fill, targetStrLen - strlen(unpadded)), unpadded);
+}
+/*
+void binaryToPaddedString(char *output, byte inputbinary) {
+  int targetStrLen = 8;           // Target output length  
+  const char *padding="00000000000000000";
+  char unpadded[20];
+  itoa (inputbinary,unpadded,2); 
+  //Serial.println(unpadded);
+  int padLen = targetStrLen - strlen(unpadded); // Calc Padding length
+  if(padLen < 0) padLen = 0;    // Avoid negative length
+  //Serial.println(padLen);
+
+  sprintf(output,"[%*.*s%s]", padLen, padLen, padding, unpadded);  // LEFT Padding 
+  Serial.println(output);
+}
+*/
+
 void setup()
 {
     Serial.begin(115200);
@@ -31,7 +60,15 @@ void setup()
     timer2_setup();
     lcd_setup();
     button_setup();
-    
+
+    byte x =  0b10111100;
+    byte y = ~x;
+    char padded[20];
+    binaryToPaddedString(padded,x);
+    Serial.println(padded);
+    binaryToPaddedString(padded,y);
+    Serial.println(padded);
+
     Serial.print("Period    = ");
     Serial.print(period); 
     Serial.println(" microseconds");
