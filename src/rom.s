2b9a573911837c2a17faf54cb4ccb561fd91624f
jon bidinger <jon@bidinger.org>
2022-10-02 00:36:41 -0500

    Many changed including what appears to be a working PLD.

--- src/rom.s
+++ src/rom.s
@@ -1,29 +1,19 @@
-; vasm -Fbin -dotdir -wdc02 rom.s
+; vasm6502_oldstyle.exe -Fbin -dotdir -wdc02 rom.s
+; xxd -a -o 32768 a.out
 
   .org $8000
-
+    nop
   .org $9000
     ;Setup stack
     ldx #$ff
     txs
-loop:
+NOPS
     nop
     nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    nop
-    jmp loop
-  .org $fffC
-  .word $0090
-  .word $0000   ; Fill to 32k
+
+  .org $fff7
+    jmp NOPS ; Go back to 9000, skipping IO region and read NOPs again.  
+  .org $fffA
+  .word $FFFF   ; NMIB
+  .word $0090   ; Jump on boot
+  .word $FFFF   ; IRQB/BRK
