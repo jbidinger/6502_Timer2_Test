cbbbd410259aa229417062ac80ffa8cc135164da
jon bidinger <jon@bidinger.org>
2022-10-03 01:42:03 -0500

    Comment changes

--- src/rom.s
+++ src/rom.s
@@ -1,8 +1,11 @@
 ; vasm6502_oldstyle.exe -Fbin -dotdir -wdc02 rom.s
 ; xxd -a -o 32768 a.out
 
-  .org $8000
-    nop
+  ; IO sections are masked by the PLD. ROM really starts at $9000
+  .org $8000    ; IO0
+  .org $8100    ; IO1
+  .org $8200    ; IO2
+  ; All the way up to $8900 but the PLD only maps 3 right now.
   .org $9000
     ;Setup stack
     ldx #$ff
