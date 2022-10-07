03c17125406d8b58bc4d3d1b0c989873ef2c32f5
jon bidinger <jon@bidinger.org>
2022-10-06 22:57:16 -0500

    RAM Test working.

--- Assembly/ram_test_hardware.s
+++ Assembly/ram_test_hardware.s
@@ -14,14 +14,34 @@ START:
     lda $01
     cmp #$FF
     bne OH_FUCK ; We stored a FF got something else back.
-    jmp START
+
+TEST_START
+    lda #$02
+    sta $00 
+    lda #$00
+    sta $01
+    ldy #$00
+    lda #$FF
+TEST_LOOP:
+    sta ($00),y
+    lda ($00)
+    cmp #$FF
+    bne OH_FUCK
+    iny             ; Counting on this to wrap from #$FF to #$00
+    cpy #$00        ; Did we wrap?
+    bne TEST_LOOP; More on this page
+    inc $01     ; Next Page. $00 always stays zero. We index with Y
+    ldx $01
+    cpx #$80    ; Stop at the end of RAM (32k in this case)
+    bne TEST_LOOP
+    jmp TEST_START
+
 
 OH_FUCK:
     lda #$00
     sta $00
     sta $01
     jmp OH_FUCK
-
 
 ;    .org $fff7
 ;        jmp START ; Go back to 9000, skipping IO region and read NOPs again.  
